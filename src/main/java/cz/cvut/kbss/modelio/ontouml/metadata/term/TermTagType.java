6152f8970509d037ea6edfeb3f51eb0900ba85b4
Petr Kremen <kremep1@fel.cvut.cz>
2020-05-20 14:35:49 +0200

    fix to labels

--- src/main/java/cz/cvut/kbss/modelio/ontouml/metadata/term/TermTagType.java
+++ src/main/java/cz/cvut/kbss/modelio/ontouml/metadata/term/TermTagType.java
@@ -4,14 +4,14 @@ import cz.cvut.kbss.modelio.ontouml.metadata.IdentifiableEnum;
 
 public enum TermTagType implements IdentifiableEnum {
 
-    TERM_NAME("sgov.term.nazev"),
-    TERM_CONTEXT_BASED_NAME("sgov.term.kontextualni-nazev"),
-    TERM_SEARCH_INDEX("sgov.term.vyhledavaci-index"),
-    TERM_MEANING_CONSTRAINT("sgov.term.vymezeni-vyznamu-pojmu"),
-    TERM_MEANING_DEFINITION("sgov.term.definice"),
-    TERM_MEANING_CONSTRAINT_SOURCE("sgov.term.odkaz-na-vymezeni-vyznamu-v-legislative"),
-    TERM_MEANING_DEFINITION_SOURCE("sgov.term.odkaz-na-vybrane-misto-v-legislative"),
-    TERM_SPECIALIZES_EXTERNAL_TERMS("sgov.term.specializuje-externi-pojmy");
+    TERM_NAME("sgov.concept.preferred-name"),
+    TERM_CONTEXT_BASED_NAME("sgov.concept.alternative-name"),
+    TERM_SEARCH_INDEX("sgov.concept.search-index"),
+    TERM_MEANING_CONSTRAINT("sgov.concept.description"),
+    TERM_MEANING_DEFINITION("sgov.concept.definition"),
+    TERM_MEANING_CONSTRAINT_SOURCE("sgov.concept.source-of-description"),
+    TERM_MEANING_DEFINITION_SOURCE("sgov.concept.source-of-definition"),
+    TERM_SPECIALIZES_EXTERNAL_TERMS("sgov.concept.specializes");
 
     private String id;
 
